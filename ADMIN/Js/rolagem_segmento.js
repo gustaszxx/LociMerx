b282025d680a7473f2960fef2b7f310b5eaefe48
Gustavo Werner <gustaszxx@gmail.com>
2024-11-11 12:00:50 -0300

    Delete ADMIN directory

--- ADMIN/Js/rolagem_segmento.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.carousel').addEventListener('wheel', function(event) {
-    if (event.deltaY !== 0) {
-        event.preventDefault();
-        this.scrollLeft += event.deltaY;
-    }
-});
